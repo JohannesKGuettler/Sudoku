9cacfe76e06785db609587ccc86b8af210909317
vanacoree <130047814+vanacoree@users.noreply.github.com>
2023-06-14 11:36:11 +0200

    Update TestNeo4j.java

--- TestNeo4j.java
+++ TestNeo4j.java
@@ -11,7 +11,6 @@ import static org.neo4j.driver.Values.parameters;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Map;
 
 public class TestNeo4j implements AutoCloseable {
