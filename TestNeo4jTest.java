88185fcf0d01ed4d64fd9f6127c6bb83956a5db2
vanacoree <130047814+vanacoree@users.noreply.github.com>
2023-06-17 10:24:04 +0200

    Add files via upload

---
+++ TestNeo4jTest.java
@@ -0,0 +1,64 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class TestNeo4jTest {
+	
+	// documentation link Neo4j https://neo4j.com/docs/aura/auradb/connecting-applications/java/
+
+	@Test
+	void testCreatePlayResult() {
+		TestNeo4j greeter = new TestNeo4j("neo4j+s://3de5149f.databases.neo4j.io", "neo4j", "RtLJnX7gMMS4slo1pqPGPpgTxqYJl2O5qvZ9wjaH1M0");
+		// to see the result being uploaded after every run please reduce the "score" (middlevalue) by one and access it through the results variable
+		greeter.createPlayResult("test", 99, "010101");
+		var Results = greeter.getBestResults("010101");
+		String name = Results[1][1].substring(1, Results[1][1].length()-1);
+		assertEquals(name,"test");
+		greeter.close();
+	}
+
+	@Test
+	void testGetDailySudoku() {
+		TestNeo4j greeter = new TestNeo4j("neo4j+s://3de5149f.databases.neo4j.io", "neo4j", "RtLJnX7gMMS4slo1pqPGPpgTxqYJl2O5qvZ9wjaH1M0");
+		int[][] downloadedDailySudoku = greeter.getDailySudoku("20220101");
+		
+		String [] testSudoku = new String[] {"[0, 1, 0, 4, 3, 0, 6, 7, 0]","[0, 3, 0, 0, 0, 0, 0, 0, 0]","[5, 0, 0, 9, 0, 0, 0, 0, 2]","[3, 5, 0, 1, 8, 0, 0, 0, 0]","[0, 6, 0, 0, 0, 0, 2, 0, 0]","[0, 0, 0, 0, 0, 0, 8, 3, 7]","[0, 2, 0, 0, 0, 7, 0, 0, 3]","[0, 0, 0, 8, 0, 3, 1, 2, 0]", "[6, 0, 0, 2, 0, 0, 0, 0, 5]"};    
+		
+		for (int i=0; i<9; i++) {
+			assertEquals(Arrays.toString(downloadedDailySudoku[i]),testSudoku[i]);
+			System.out.println(Arrays.toString(downloadedDailySudoku[i]));
+		}
+		// this sudoku should look like the following
+		// row1: [0, 1, 0, 4, 3, 0, 6, 7, 0]
+		// row2: [0, 3, 0, 0, 0, 0, 0, 0, 0]
+		// row3: [5, 0, 0, 9, 0, 0, 0, 0, 2]
+		// row4: [3, 5, 0, 1, 8, 0, 0, 0, 0]
+		// row5: [0, 6, 0, 0, 0, 0, 2, 0, 0]
+		// row6: [0, 0, 0, 0, 0, 0, 8, 3, 7]
+		// row7: [0, 2, 0, 0, 0, 7, 0, 0, 3]
+		// row8: [0, 0, 0, 8, 0, 3, 1, 2, 0]
+		// row9: [6, 0, 0, 2, 0, 0, 0, 0, 5]
+		greeter.close();
+	}
+
+	@Test
+	void testGetBestResults() {
+		TestNeo4j greeter = new TestNeo4j("neo4j+s://3de5149f.databases.neo4j.io", "neo4j", "RtLJnX7gMMS4slo1pqPGPpgTxqYJl2O5qvZ9wjaH1M0");
+		var Results = greeter.getBestResults("111062023");
+		
+		// Results will have 11 arrays: first array are min/max/avg value of players on this date, remaining 10 are player records [[playtime][username]]
+		// the array checks that the values are not null
+		assertTrue(Results[0][0] != null);
+		assertTrue(Results[0][1] != null);
+		assertTrue(Results[0][2] != null);
+		for (int i=1; i<11; i++) {
+			assertTrue(Results[i][0] != null);
+			assertTrue(Results[i][1] != null);
+		}
+		
+		// best results for this example date should look like the following -> first value: playtime, second value: playername
+		greeter.close();
+	}
+}
